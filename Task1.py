04d37d01accf5bfc7b73b3fd4cf9d9993e4b5244
sivansalzmann <sivansalzmann10@gmail.com>
2021-01-23 13:06:57 +0200

    add files

---
+++ Task1.py
@@ -0,0 +1,14 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+from sklearn.naive_bayes import GaussianNB
+import datetime
+from pandas.api.types import is_numeric_dtype
+import sklearn as skl
+from scipy.stats import skewnorm
+import scipy.stats as stats
+from sklearn.tree import DecisionTreeClassifier # Import Decision Tree Classifier
+from sklearn.model_selection import train_test_split # Import train_test_split function
+from sklearn import metrics #Import scikit-learn metrics module for accuracy calculation
+
